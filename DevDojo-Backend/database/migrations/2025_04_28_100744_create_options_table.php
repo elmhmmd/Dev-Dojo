642ef0bbd8eb75a61218bb7129bbefa131a8f705
elmhmmd <youcodemohammed@gmail.com>
2025-04-28 11:53:17 +0100

    Modifying Questions Migration

--- DevDojo-Backend/database/migrations/2025_04_28_100744_create_options_table.php
+++ DevDojo-Backend/database/migrations/2025_04_28_100744_create_options_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('options', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('question_id');
+            $table->text('body');
+            $table->boolean('is_correct')->default(false);
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
