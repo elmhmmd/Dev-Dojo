f0a3b44bd84ad27f4b7d83406a1ffa19729f36ac
elmhmmd <youcodemohammed@gmail.com>
2025-04-28 12:04:03 +0100

    Modifying KeyLearningObjective Migration

--- DevDojo-Backend/database/migrations/2025_04_28_100746_create_key_learning_objectives_table.php
+++ DevDojo-Backend/database/migrations/2025_04_28_100746_create_key_learning_objectives_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('key_learning_objectives', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('node_id');
+            $table->text('body');
+            $table->foreign('node_id')->references('id')->on('nodes')->onDelete('cascade');
             $table->timestamps();
         });
     }
