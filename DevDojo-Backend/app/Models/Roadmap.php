c8c38d55f157e9807854ea34f905f51f415690d4
elmhmmd <youcodemohammed@gmail.com>
2025-04-28 11:10:42 +0100

    Creating Roadmap Model

---
+++ DevDojo-Backend/app/Models/Roadmap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Roadmap extends Model
+{
+    protected $fillable = ['title', 'created_by'];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function nodes()
+    {
+        return $this->hasMany(Node::class);
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(User::class, 'roadmap_user');
+    }
+}
