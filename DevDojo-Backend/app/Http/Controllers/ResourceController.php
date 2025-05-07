7bbc9b470a180fde6efc493e4825f92e657b7f83
elmhmmd <youcodemohammed@gmail.com>
2025-05-07 16:01:50 +0100

    Modifying ResourceController

--- DevDojo-Backend/app/Http/Controllers/ResourceController.php
+++ DevDojo-Backend/app/Http/Controllers/ResourceController.php
@@ -9,12 +9,23 @@ class ResourceController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:api', 'admin']);
+        $this->middleware(['auth:api', 'admin'])->except(['index']);
     }
 
     public function index($roadmapId, $nodeId)
     {
+        $user = auth()->user();
+
+        $node = Node::with('roadmap')->findOrFail($nodeId);
+
+        if ($user->role_id !== 1 && ! $node->roadmap->published) {
+            return response()->json([
+                'error' => 'You do not have access to these resources'
+            ], 403);
+        }
+
         $resources = Resource::where('node_id', $nodeId)->get();
+
         return response()->json($resources, 200);
     }
 
