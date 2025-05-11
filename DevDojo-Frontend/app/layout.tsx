6a7f721cd4f0014db4babdfe51b9cccec9334ef4
elmhmmd <youcodemohammed@gmail.com>
2025-05-11 15:20:07 +0100

    Adding Root Layout Componenet

---
+++ DevDojo-Frontend/app/layout.tsx
@@ -0,0 +1,34 @@
+import type React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
+import { AuthProvider } from "@/providers/auth-provider"
+import "@/app/globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "DevDojo - Master Programming",
+  description: "Master programming, one step at a time.",
+    generator: 'devdojo'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <AuthProvider>
+            {children}
+            <Toaster />
+          </AuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
